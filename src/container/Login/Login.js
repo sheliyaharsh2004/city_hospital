e5e68f50107ac8cc1d62859072da82fcf6a3c70b
sheliyaharsh2004 <radhikakachhadiya823@gmail.com>
2022-05-25 10:01:02 +0530

    login

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import * as yup from 'yup';
 
 function Login(props) {
 
     const [userType, setuserType] = useState('Login')
+    const [Reset, setReset] = useState(false)
+
+    const login = {
+        name: yup.string().required(),
+        email: yup.string().email(),
+    }   ;
 
     return (
         <main id="main">
@@ -20,7 +27,7 @@ function Login(props) {
                     <form action method="post" role="form" classname="php-email-form">
                         <div classname="row flex-column align-items-center">
                             {
-                                React ?
+                                Reset ?
                                     <div className="col-md-4 mt-3 form-group m-auto">
                                         <input type="text" name="email" className="form-control" id="email" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                         <div className="validate" />
@@ -48,23 +55,23 @@ function Login(props) {
                         </div>
                         {
                             userType === 'Login' ?
-                            <>
-                                <div className="text-center"><button className='appointment-btn border-0 m-0' type="button">Login</button></div>
-                                <div className="text-center"><button className='appointment-btn border-0 m-0 mt-3' type="button" onClick={() => setResrt(true)}>forgot password</button></div>
-                            </>
-                            :
-                            <div className="text-center"><button className='appointment-btn border-0 m-0' type="button">Signup</button></div>
+                                <>
+                                    <div className="text-center"><button className='appointment-btn border-0 m-0 mt-3' type="button">Login</button></div>
+                                    <div className="text-center"><button className='appointment-btn border-0 m-0 mt-3' type="button" onClick={() => setReset(true)}>forgot password</button></div>
+                                </>
+                                :
+                                <div className="text-center"><button className='appointment-btn border-0 m-0' type="button">Signup</button></div>
                         }
                         <div className='row'>
                             {
                                 userType === 'Login' ?
-                                <div className='text-center pt-4'>
-                                    <p>Create a new account</p><button className='appointment-btn border-0 m-0' type="button" onClick={() => setuserType('Signup')}>Sign Up</button>
-                                </div>
-                                :
-                                <div className='text-center pt-4'>
-                                    <p>Create a new account</p><button className='appointment-btn border-0 m-0' type="button" onClick={() => setuserType('Login')}>Login</button>
-                                </div>
+                                    <div className='text-center pt-4'>
+                                        <p>Create a new account</p><button className='appointment-btn border-0 m-0' type="button" onClick={() => setuserType('Signup')}>Sign Up</button>
+                                    </div>
+                                    :
+                                    <div className='text-center pt-4'>
+                                        <p>Create a new account</p><button className='appointment-btn border-0 m-0' type="button" onClick={() => setuserType('Login')}>Login</button>
+                                    </div>
                             }
                         </div>
                     </form>
