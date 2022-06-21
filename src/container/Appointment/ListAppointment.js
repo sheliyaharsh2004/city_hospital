0dd6be1f176002b38b006edae63895fcf3034db5
sheliyaharsh2004 <hp0111258@gmail.com>
2022-06-21 09:02:44 +0530

    new

--- src/container/Appointment/ListAppointment.js
+++ src/container/Appointment/ListAppointment.js
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import TextField from "@mui/material/TextField";
+import { DataGrid } from "@mui/x-data-grid";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { useState } from 'react';
 
 function ListAppointment(props) {
@@ -12,11 +17,47 @@ function ListAppointment(props) {
         setData(listData);
     }
      
+    const handleClickDopen = (id) => {
+       
+    };
+
+    const handleClickEditOpen = (params) => {
+    };
+
     useEffect(
         () =>{
             showdata()
         },
     []);
+
+    const columns = [
+        { field: "name", headerName: "Name", width: 130 },
+        { field: "email", headerName: "Email", width: 130 },
+        { field: "phone", headerName: "Phone", width: 130 },
+        { field: "date", headerName: "Date", width: 130 },
+        { field: "department", headerName: "Department", width: 130 },
+        { field: "message", headerName: "Message", width: 130 },
+        {
+          field: "action",
+          headerName: "Action",
+          width: 100,
+          renderCell: (params) => {
+            return (
+              <>
+                <IconButton
+                  className="border-primary"
+                  onClick={() => handleClickDopen(params.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+                <IconButton onClick={() => handleClickEditOpen(params)}>
+                  <EditIcon />
+                </IconButton>
+              </>
+            );
+          },
+        },
+    ];
 
     return (
         <main id="main">
@@ -38,16 +79,14 @@ function ListAppointment(props) {
                             </div>
                         </div>
                     </div>
-                    <div>
-                        {
-                            data.map((d,i) =>{
-                                return(
-                                    <>
-                                        <h4>{d.name}</h4>
-                                    </>
-                                )
-                            })
-                        }
+                    <div className="mt-3" style={{ height: 400, width: "100%" }}>
+                        <DataGrid
+                            rows={data}
+                            columns={columns}
+                            rowsPerPageOptions={[5]}
+                            pageSize={5}
+                            checkboxSelection
+                        />
                     </div>
                 </div>
             </section>
